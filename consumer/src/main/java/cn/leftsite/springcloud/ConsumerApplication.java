68f0732d7d8dbdb35041e2809018d85b87b21027
wangjinqiang <1061669148@qq.com>
2020-05-17 14:28:41 +0800

    hystrix turbine

--- consumer/src/main/java/cn/leftsite/springcloud/ConsumerApplication.java
+++ consumer/src/main/java/cn/leftsite/springcloud/ConsumerApplication.java
@@ -4,6 +4,8 @@ import cn.leftsite.springcloud.feign.ProducerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringCloudApplication
 @EnableFeignClients
+@EnableHystrixDashboard
+@EnableTurbine
 public class ConsumerApplication {
     @Autowired
     private ProducerFeignClient producerFeignClient;
