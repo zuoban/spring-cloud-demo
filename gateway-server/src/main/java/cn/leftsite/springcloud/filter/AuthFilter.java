806261571fc87b041937982de68714eefb500b4d
wangjinqiang <1061669148@qq.com>
2020-05-18 22:46:55 +0800

    gateway 过滤器

---
+++ gateway-server/src/main/java/cn/leftsite/springcloud/filter/AuthFilter.java
@@ -0,0 +1,29 @@
+package cn.leftsite.springcloud.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getQueryParams().getFirst("access-token");
+        if (token == null) {
+            System.out.println("没有登录");
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        System.out.println("执行自定义全局过滤器");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
