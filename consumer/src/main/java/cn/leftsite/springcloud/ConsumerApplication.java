181a749a6c941dc1c3458456aa9378cb92bd3ce7
wangjinqiang <1061669148@qq.com>
2020-05-18 15:03:41 +0800

    sentinel test

--- consumer/src/main/java/cn/leftsite/springcloud/ConsumerApplication.java
+++ consumer/src/main/java/cn/leftsite/springcloud/ConsumerApplication.java
@@ -1,11 +1,11 @@
 package cn.leftsite.springcloud;
 
 import cn.leftsite.springcloud.feign.ProducerFeignClient;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringCloudApplication
 @EnableFeignClients
-@EnableHystrixDashboard
-@EnableTurbine
 public class ConsumerApplication {
     @Autowired
     private ProducerFeignClient producerFeignClient;
 
     @GetMapping
+    @SentinelResource(value = "home", blockHandler = "homeBlockHandler", fallback = "homeFallback")
     public String home() {
         return producerFeignClient.home();
+    }
+
+    @GetMapping("timeout")
+    @SentinelResource(value = "home", blockHandler = "homeBlockHandler", fallback = "homeFallback")
+    public String timeout() {
+        return producerFeignClient.timeout();
+    }
+
+
+    public String homeBlockHandler(BlockException e) {
+        return "homeBlockHandler" + e.getMessage();
+    }
+
+    public String homeFallback(Throwable t) {
+        return "homeFallback" + t.getMessage();
     }
 
     public static void main(String[] args) {
--- consumer/src/main/java/cn/leftsite/springcloud/feign/ProducerFeignClient.java
+++ consumer/src/main/java/cn/leftsite/springcloud/feign/ProducerFeignClient.java
@@ -3,8 +3,11 @@ package cn.leftsite.springcloud.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "producer", fallback = ProducerFeignClientFallback.class)
+@FeignClient(value = "producer")
 public interface ProducerFeignClient {
     @GetMapping("/")
     String home();
+
+    @GetMapping("/timeout")
+    String timeout();
 }
