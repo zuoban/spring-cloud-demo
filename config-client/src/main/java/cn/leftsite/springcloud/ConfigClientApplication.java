d7c3ec1bee1db3d775f73d52c1c48f5bbf4d6527
wangjinqiang <1061669148@qq.com>
2020-05-22 23:09:29 +0800

    Spring Cloud Config 入门

---
+++ config-client/src/main/java/cn/leftsite/springcloud/ConfigClientApplication.java
@@ -0,0 +1,24 @@
+package cn.leftsite.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+    @Value("${config.info}")
+    private String info;
+
+    @GetMapping
+    public String home() {
+        return "Hello World! " + info;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
