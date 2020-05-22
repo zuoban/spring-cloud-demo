92a40e0127fdbafeb54167e5dff4ea29c0197b07
wangjinqiang <1061669148@qq.com>
2020-05-22 18:47:40 +0800

    消息分区

--- stream-producer/src/test/java/cn/leftsite/springcloud/service/MessageSenderTest.java
+++ stream-producer/src/test/java/cn/leftsite/springcloud/service/MessageSenderTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.stream.IntStream;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class MessageSenderTest {
@@ -14,8 +16,10 @@ public class MessageSenderTest {
 
     @Test
     public void send() {
-        for (int i = 0; i < 10; i++) {
-            messageSender.send("hello , ~ " + (i + 1));
+        for (int i = 0; i < 3; i++) {
+            IntStream.range(0, 5).forEach(it -> {
+                messageSender.send("hello ~~~ " + it);
+            });
         }
     }
 }
