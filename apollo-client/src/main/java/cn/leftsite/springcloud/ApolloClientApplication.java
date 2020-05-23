e41bb53b5d1e338f7d6566d94ffd0fa45d9a405f
wangjinqiang <1061669148@qq.com>
2020-05-24 00:33:01 +0800

    apollo

---
+++ apollo-client/src/main/java/cn/leftsite/springcloud/ApolloClientApplication.java
@@ -0,0 +1,23 @@
+package cn.leftsite.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApolloClientApplication {
+    @Value("${name:default}")
+    public String name;
+
+    @GetMapping
+    public String home() {
+        return "hello " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloClientApplication.class, args);
+    }
+}
